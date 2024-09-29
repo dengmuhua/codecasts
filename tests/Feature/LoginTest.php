36adb87291481b2a949f5b5e053ef88a52f07c61
woody_deng <woody_deng@163.com>
2024-09-29 11:10:22 +0800

    登录和注册单例测试功能

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -5,20 +5,12 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
 
     protected function data(){
         $user =User::factory()->create();
@@ -28,16 +20,31 @@ class LoginTest extends TestCase
         ];
     }
 
-    /**
-     * 登录
-     * @test
-     * @return void
-     */
+    #[Test]
     public function loginTest(){
         $this->withoutExceptionHandling();
         $user = User::factory()->create();
         //$response = $this->post('/api/login', ['account' => $user->email, 'password' => $user->password]);
         $response = $this->post('/api/login', $this->data());
         $response->assertSee('token');
+    }
+
+    #[Test] // Email Illegal Test[邮箱不合法]
+    public function loginEmailIllegal(){
+        $response = $this->post('/api/login', ['account' => 'illegal_email', 'password' => 'password']);
+        $response->assertSessionHasErrors('account');
+    }
+
+    #[Test] // Password Input Wrong Test[密码输入错误]
+    public function passwordInputWrong(){
+        $user =User::factory()->create();
+        $response = $this->post('/api/login', ['account' => $user->email, 'password' => 'wrong_password']);
+        $response->assertSessionHasErrors('password');
+    }
+
+    #[Test] // Account Not Exist Test[账户不存在]
+    public function accountNotExist(){
+        $response = $this->post('/api/login', ['account' => '3345678@qq.com', 'password' => 'password']);
+        $response->assertSessionHasErrors('account');
     }
 }
--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -5,11 +5,12 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 class RegisterTest extends TestCase
 {
-    //use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
     protected function data(){
         $user = User::factory()->make();
@@ -23,24 +24,51 @@ class RegisterTest extends TestCase
         ];
     }
 
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-
-    /**
-     * 用户注册
-     * @test
-     * @return void
-     */
+    #[Test] // user register test[用户注册]
     public function userRegister(){
         $response = $this->post('/api/register', $this->data());
         $response->assertSuccessful();
+    }
+
+    #[Test] // account required validation[账号不能为空]
+    public function accountRequiredValidation(){
+        $data = $this->data();
+        unset($data['account']);
+        $response = $this->postJson('/api/register', $data);
+        $response->assertJsonValidationErrors(['account']);
+    }
+
+    #[Test] // captcha input error[错误的验证码]
+    public function captchaInputError(){
+        $response = $this->postJson('/api/register', ['code' => '334567'] +$this->data());
+        $response->assertJsonValidationErrors(['code']);
+    }
+
+    #[Test] // register account validation[非法邮箱验证]
+    public function registerAccountValidation(){
+        $response = $this->postJson('/api/register', ['account' => 'test.com'] +$this->data());
+        $response->assertJsonValidationErrors(['account']);
+    }
+
+    #[Test] // account unique validation[账号已存在]
+    public function accountUniqueValidation(){
+        $data = $this->data();
+        $this->postJson('/api/register', $data);
+        $response2 = $this->postJson('/api/register', $data);
+        $response2->assertJsonValidationErrors(['account']);
+    }
+
+    #[Test] // input password error[密码有误]
+    public function inputPasswordError(){
+        $response = $this->postJson('/api/register', ['password' => ''] +$this->data());
+        $response->assertJsonValidationErrors(['password']);
+    }
+
+    #[Test] // Login by mobile[手机登录]
+    public function loginByMobile(){
+        $user = User::factory()->create(['phone' => '136' . $this->faker()->randomNumber(8)]);
+        $response = $this->postJson('/api/login', ['account' => $user->phone, 'password' => 'password']);
+        $response->assertOk();
     }
 
 }
